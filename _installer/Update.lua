ef92e0f2e1561d9c58808e8896943c8ea95105a2
KilDoom <117968565+KilDoomWise@users.noreply.github.com>
2025-03-25 13:08:18 +0300

    Update Update.lua

--- _installer/Update.lua
+++ _installer/Update.lua
@@ -57,7 +57,7 @@ do
 end
 
 -- URL к вашему установщику
-local installerUrl = "https://raw.githubusercontent.com/KilDoomWise/InfluxOS/master/Installer/Main.lua"
+local installerUrl = "https://raw.githubusercontent.com/KilDoomWise/InfluxOS/main/Installer/Main.lua"
 
 -- Проверка доступности сервера
 do
